c9ce1915ca52f49b7878ba38763e9576b0d2736e
unknown <HELI@EAW12DD703.esko-graphics.com>
2014-07-17 14:30:25 +0800

    add spider for suning, but the price is not parsed becaused it's rendered by png

--- kidscare/spiders/milk_suning.py
+++ kidscare/spiders/milk_suning.py
@@ -19,11 +19,14 @@ class SUNINGMilk_Spider(MilkSpider):
     name = "suning"
     #allowed_domains = ["http://www.qunar.com/"]
     start_urls = [
-        "http://dujia.qunar.com/tejia",
+        "http://list.suning.com/0-313006-0-1-0-9264.html",
+        "http://list.suning.com/0-313007-0-1-0-9264.html",
+        "http://list.suning.com/0-313008-0-1-0-9264.html",
+        "http://list.suning.com/0-313009-0-1-0-9264.html",
     ]
     
     def __init__(self):
-        super(MilkSpider, self).__init__()
+        super(SUNINGMilk_Spider, self).__init__()
     
     def parse(self, response):
         """
@@ -32,7 +35,43 @@ class SUNINGMilk_Spider(MilkSpider):
         @scrapes name
         """
         #sel = Selector(response)
-        pass
+        sel = Selector(None, response.body_as_unicode().replace('\t','').replace('\r','').replace('\n',''), 'html') #avoid the html contain "\n", "\r" , which will caused the xpath doesn't work well
+        prodlist_node = sel.xpath('//div[@id="proShow"]')
+        listdata = prodlist_node.xpath('ul/li')
+        
+        item = Milk()
+        for prod in listdata:
+            try:
+                prod_link = prod.xpath('a/@href').extract()[0]
+                pic_link = prod.xpath('a/img/@src | a/img/@src2').extract()[0]
+                info_node = prod.xpath('div[@class="inforBg"]')
+                price = info_node.xpath('p[@class="price"]/img/@src | p[@class="price"]/img/@src2').extract()[0] #price image link
+                item["prod_link"] = prod_link
+                item["pic_link"] = pic_link
+                item["price"] = price
+                title = info_node.xpath('span/a/p/text()').extract()[0]
+                dict = super(SUNINGMilk_Spider, self).ParseTitleToDict(title)
+                item["name"] = dict["name"]
+                item["brand"] = dict["brand"]
+                item["segment"] = dict["segment"]
+                item["volume"] = dict["volume"]
+                #item["unitprice"] = item["price"] / dict["volume"] * 100.0
+                #if item["unitprice"] < 90 and item["unitprice"] > 10:
+                yield item
+                    
+            except Exception, info: #IndexError
+                s=sys.exc_info()                             
+                log.msg("[dangdang_milk] Error '%s' happened on line %d" % (s[1],s[2].tb_lineno), log.ERROR)
+                #log.msg('[jd_milk] prod_link : %s' % prod_link, log.ERROR)
+                log.msg('[dangdang_milk] item : %s' % item, log.ERROR)
+        
+        nextpage_node = sel.xpath('//a[@id="nextPage"]/@href')
+        if not nextpage_node:
+            return
+        else:
+            nextpage_link = "http://list.suning.com" + nextpage_node.extract()[0]
+        
+        yield Request(url=nextpage_link, callback=self.parse)
         
     
     def __unicode__(self):
