cf203eb2a77b13c7888d9ae48578ef476ead7756
AutoCoder <tj_liyuan@163.com>
2014-09-09 14:42:03 +0800

    update windows database ip

--- kidscare/pipelines.py
+++ kidscare/pipelines.py
@@ -22,9 +22,9 @@ else:
     if platform.system() is 'Windows':
         DbHost = '127.0.0.1'
     elif platform.system() in ('Linux',):
-        DbHost = '10.31.186.144'
+        DbHost = '10.31.186.120'
     else:
-        DbHost = '10.31.186.144'
+        DbHost = '10.31.186.120'
 
 class KidscarePreprocessPipeline(object):
     """
