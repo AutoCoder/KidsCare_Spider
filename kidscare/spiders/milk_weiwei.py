0ff38802c16a38cf490601bf9435198d771d0412
AutoCoder <tj_liyuan@163.com>
2014-10-16 16:43:04 +0800

    fix the weiwei spider doesn't work

--- kidscare/spiders/milk_weiwei.py
+++ kidscare/spiders/milk_weiwei.py
@@ -5,8 +5,6 @@ Created on 2014.7.17
 '''
 from scrapy.selector import Selector
 from scrapy.http import Request
-
-#from spiderutility import SpiderUtility
 from kidscare.items import Milk
 from kidscare.spiders.basespider import MilkSpider
 from scrapy import log
@@ -31,20 +29,18 @@ class WWMilk_Spider(MilkSpider):
         @url http://dujia.qunar.com/tejia
         @scrapes name
         """
-        #sel = Selector(response)
         sel = Selector(None, response.body_as_unicode().replace('\t','').replace('\r','').replace('\n',''), 'html') #avoid the html contain "\n", "\r" , which will caused the xpath doesn't work well
-        listdata = sel.xpath('//ul[@class="goods_grid2"]/li')
-        
+        listdata = sel.xpath('//div[@class="list_prot"]/ul/li')
         item = Milk()
         for prod in listdata:
             try:
-                title = prod.xpath('p[@class="img"]/a/img/@title').extract()[0]
-                pic_link = prod.xpath('p[@class="img"]/a/img/@original').extract()[0]
-                prod_link = "http://" + self.allowed_domains[0] + prod.xpath('p[@class="img"]/a/@href').extract()[0]
-                price = prod.xpath('p[@class="price"]/span/text()').extract()[0][1:]
+                price_str = prod.xpath('div/div[@class="shop_price"]/span[@class="mo_b"]/text()').extract()[0][1:]
+                title = prod.xpath('div/div[@class="introduction"]/a/@title').extract()[0]
+                prod_link = self.allowed_domains[0] + prod.xpath('div/div[@class="shop_appearance"]/a/@href').extract()[0]
+                pic_link = self.allowed_domains[0] + prod.xpath('div/div[@class="shop_appearance"]/a/img/@src').extract()[0]
                 item["prod_link"] = prod_link
                 item["pic_link"] = pic_link
-                item["price"] = float(price)                
+                item["price"] = float(price_str)                
                 dict = super(WWMilk_Spider, self).ParseTitleToDict(title)
                 item["name"] = dict["name"]
                 item["brand"] = dict["brand"]
